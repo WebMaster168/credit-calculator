afaf3cd3551d1476a1a0bcd4138e6dcbdaad0b4e
Ilya Shcherbakov <shcherbakov.illya@list.ru>
2023-12-17 20:23:07 +0300

    send-letter

--- js/controller.js
+++ js/controller.js
@@ -76,4 +76,89 @@ window.onload = function () {
             sliderTime.noUiSlider.set(data.time)
         }
     }
+
+
+    const openFormBtn = document.querySelector("#openFormBtn")
+    const orderForm = document.querySelector("#orderForm")
+    const submitFormBtn = document.querySelector("#submitFormBtn")
+
+    openFormBtn.addEventListener('click', function(){
+        orderForm.classList.remove('none')
+        openFormBtn.classList.add('none')
+    })
+
+    orderForm.addEventListener('submit', function(e){
+        e.preventDefault()
+        const formData = new FormData(orderForm)
+
+        submitFormBtn.setAttribute('disabled', true)
+        submitFormBtn.innerText = "Заявка отправляется..."
+
+        orderForm.querySelectorAll('input').forEach(function (input){
+            input.setAttribute('disabled', true)
+        })
+
+        fetchData()
+        async function fetchData(){
+            const data = Model.getData()
+            const resultsData = Model.getResults()
+
+            let url = document.location.href
+            url = checkOnUrl(url)
+            console.log(url)
+            function checkOnUrl(url){
+                let urlArrayDot = url.split('.')
+                if(urlArrayDot[urlArrayDot.length - 1] === 'html'){
+                    urlArrayDot.pop()
+
+                    let newUrl = urlArrayDot.join('.')
+
+                    let urlArraySlash = newUrl.split('/')
+
+                    urlArraySlash.pop()
+
+                    newUrl = urlArraySlash.join('/') + '/'
+
+                    return newUrl
+                }
+
+                return url
+                
+            }
+
+            const response = await fetch(url+'mail.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify({
+                    form:{
+                        name: formData.get('name'),
+                        email: formData.get('email'),
+                        phone: formData.get('phone')
+                    },
+                    data,
+                    resultsData
+                })
+            })
+            const result = await response.text()
+            console.log(result)
+
+            submitFormBtn.removeAttribute('disabled', true)
+            submitFormBtn.innerText = "Оформить заявку"
+            orderForm.querySelectorAll('input').forEach((input) => {
+                input.removeAttribute('disabled', true)
+            })
+
+            orderForm.reset()
+            orderForm.classList.add('none')
+
+            if(result === 'SUCCESS'){
+                document.getElementById('success').classList.remove('none')
+            }else{
+                document.getElementById('error').classList.remove('none')
+             
+            }
+        }
+    })
 }
